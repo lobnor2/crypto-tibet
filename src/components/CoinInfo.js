43ba0424a9a09663e9391efad0640eee90af9268
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2022-07-01 23:38:22 +0530

    added single coin page CoinInfo

---
+++ src/components/CoinInfo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CoinInfo = () => {
+  return <div>CoinInfo this is mine</div>;
+};
+
+export default CoinInfo;
--- src/pages/Coinpage.js
+++ src/pages/Coinpage.js
@@ -1,7 +1,41 @@
-import React from "react";
+import { makeStyles } from "@material-ui/core";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import CoinInfo from "../components/CoinInfo";
+import { SingleCoin } from "../config/api";
+import { CryptoState } from "../CryptoContext";
 
 const Coinpage = () => {
-  return <div>Coinpage</div>;
+  // return id variable from url
+  const { id } = useParams();
+  // to store whatever we get from api
+  const [coin, setCoin] = useState();
+  // currency and symbol are from context api
+  const { currency, symbol } = CryptoState();
+
+  const fetchCoin = async () => {
+    const { data } = await axios.get(SingleCoin(id));
+    setCoin(data);
+  };
+
+  useEffect(() => {
+    fetchCoin();
+  }, []);
+
+  const useStyles = makeStyles(() => ({}));
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.sidebar}>{/* sidebar */}</div>
+      <div className={classes.chart}>
+        {/* chart */}
+        <CoinInfo coin={coin} />
+      </div>
+    </div>
+  );
 };
 
 export default Coinpage;
