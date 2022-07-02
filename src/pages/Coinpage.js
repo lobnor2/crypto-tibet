2c7b02e4544b122dbda9f4d650780e90b8321586
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2022-07-02 21:15:23 +0530

    added coininfo and descriptioin

--- src/pages/Coinpage.js
+++ src/pages/Coinpage.js
@@ -1,4 +1,4 @@
-import { makeStyles } from "@material-ui/core";
+import { LinearProgress, makeStyles, Typography } from "@material-ui/core";
 import axios from "axios";
 import React, { useEffect } from "react";
 import { useState } from "react";
@@ -6,6 +6,8 @@ import { useParams } from "react-router-dom";
 import CoinInfo from "../components/CoinInfo";
 import { SingleCoin } from "../config/api";
 import { CryptoState } from "../CryptoContext";
+import ReactHtmlParser from "react-html-parser";
+import { numberWithCommas } from "../components/Banner/Carousel";
 
 const Coinpage = () => {
   // return id variable from url
@@ -24,16 +26,91 @@ const Coinpage = () => {
     fetchCoin();
   }, []);
 
-  const useStyles = makeStyles(() => ({}));
+  const useStyles = makeStyles((theme) => ({
+    container: {
+      // container will have flex when display is full screen
+      display: "flex",
+      // if less than md then display column
+      [theme.breakpoints.down("md")]: {
+        flexDirection: "column",
+        alignItems: "center",
+      },
+    },
+    sidebar: {
+      width: "30%",
+      [theme.breakpoints.down("md")]: {
+        width: "100%",
+      },
+      display: "flex",
+      flexDirection: "center",
+      aligntItems: "center",
+      marginTop: 25,
+      borderRight: "2px soid grey",
+    },
+    heading: {
+      fontWeight: "bold",
+      marginBottom: 20,
+    },
+    description: {
+      width: "100%",
+      padding: 25,
+      paddingBottom: 15,
+      paddingTop: 0,
+      textAlign: "justify",
+    },
+  }));
   const classes = useStyles();
+
+  if (!coin) return <LinearProgress style={{ backgroundColor: "gold" }} />;
 
   return (
     <div className={classes.container}>
-      <div className={classes.sidebar}>{/* sidebar */}</div>
-      <div className={classes.chart}>
-        {/* chart */}
-        <CoinInfo coin={coin} />
+      <div className={classes.sidebar}>
+        <img
+          src={coin?.image.large}
+          alt={coin?.name}
+          height="200"
+          style={{ marginBottom: 20 }}
+        />
+        <Typography variant="h3" className={classes.heading}>
+          {coin?.name}
+        </Typography>
+        {/* for descriptions */}
+        <Typography>
+          {ReactHtmlParser(coin?.description.en.split(". ")[0])}
+        </Typography>
+        <div className={classes.marketData}>
+          <span style={{ display: "flex" }}>
+            <Typography variant="h4" className={classes.heading}>
+              Rank:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography variant="h5">{coin?.market_cap_rank}</Typography>
+          </span>
+          <span style={{ display: "flex" }}>
+            <Typography variant="h4" className={classes.heading}>
+              Current Price:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography variant="h5">
+              {symbol}{" "}
+              {numberWithCommas(
+                coin?.market_data.current_price[currency.toLowerCase()]
+              )}
+            </Typography>
+          </span>
+          <span style={{ display: "flex" }}>
+            <Typography variant="h4" className={classes.heading}>
+              Rank:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography variant="h5">{coin?.market_cap_rank}</Typography>
+          </span>
+        </div>
       </div>
+
+      {/* chart */}
+      <CoinInfo coin={coin} />
     </div>
   );
 };
