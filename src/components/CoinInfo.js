83a48fad6a22da1d1f546f681f61fbfbd4e5c665
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2022-07-09 23:32:32 +0530

    added darktheme and line chart in coin info

--- src/components/CoinInfo.js
+++ src/components/CoinInfo.js
@@ -1,7 +1,71 @@
+import {
+  CircularProgress,
+  createTheme,
+  ThemeProvider,
+} from "@material-ui/core";
+import axios from "axios";
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { HistoricalChart } from "../config/api";
+import { CryptoState } from "../CryptoContext";
 
-const CoinInfo = () => {
-  return <div>CoinInfo this is mine</div>;
+const CoinInfo = ({ coin }) => {
+  const [historicData, setHistoricData] = useState();
+  const [days, setDays] = useState(1);
+  const { currency } = CryptoState();
+
+  const fetchHistoricData = async () => {
+    const { data } = await axios.get(HistoricalChart(coin.id, days, currency));
+    setHistoricData(data.prices);
+  };
+
+  useEffect(() => {
+    fetchHistoricData();
+  }, [currency, days]);
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
+  const useStyles = makeStyles((theme) => ({
+    container: {
+      width: "75%",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 25,
+      padding: 40,
+      [theme.breakpoints.down("md")]: {
+        width: "100%",
+        marginTop: 0,
+        padding: 20,
+        paddingTop: 0,
+      },
+    },
+  }));
+
+  const classes = useStyles();
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <div className={classes.container}>
+        {/* chart */}
+        {!historicData ? (
+          <CircularProgress style={{ color: "gold" }} size={250} tickness={1} />
+        ) : (
+          <> </>
+        )}
+
+        {/* button */}
+      </div>
+    </ThemeProvider>
+  );
 };
 
 export default CoinInfo;
