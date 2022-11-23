a48b57d6dc8f7c85a815e56e3f469cf8bd8a46f4
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2022-11-23 10:00:41 +0530

    header completed and working on context api

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import {
   AppBar,
   Container,
+  createTheme,
   makeStyles,
   MenuItem,
   Select,
+  ThemeProvider,
   Toolbar,
   Typography,
 } from "@material-ui/core";
@@ -21,28 +23,44 @@ const useStyles = makeStyles(() => ({
 const Header = () => {
   const navigate = useNavigate();
   const classes = useStyles();
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
   return (
-    <AppBar color="transparent" position="static">
-      <Container>
-        <Toolbar>
-          <Typography className={classes.title} onClick={() => navigate("/")}>
-            Crypto Tibet
-          </Typography>
-          <Select
-            label="Currency"
-            variant="outlined"
-            style={{
-              width: 100,
-              height: 40,
-              marginLeft: 15,
-            }}
-          >
-            <MenuItem value={"USD"}>USD</MenuItem>
-            <MenuItem value={"INR"}>INR</MenuItem>
-          </Select>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              className={classes.title}
+              onClick={() => navigate("/")}
+              variant="h5"
+            >
+              Crypto Tibet
+            </Typography>
+            <Select
+              label="Currency"
+              variant="outlined"
+              style={{
+                width: 100,
+                height: 40,
+                marginLeft: 15,
+              }}
+            >
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"INR"}>INR</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
   );
 };
 
